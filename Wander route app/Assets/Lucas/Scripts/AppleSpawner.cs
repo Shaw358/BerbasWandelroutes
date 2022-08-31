2349e1c806b6908ea6aea07cafd4c7dad435d380
CMDR <26555@ma-web.nl>
2022-08-31 14:43:50 +0200

    Finalised apple minigame

--- Wander route app/Assets/Lucas/Scripts/AppleSpawner.cs
+++ Wander route app/Assets/Lucas/Scripts/AppleSpawner.cs
@@ -9,6 +9,8 @@ public class AppleSpawner : MonoBehaviour
 
     [SerializeField] GameObject applePrefab;
 
+    [SerializeField] Vector2 spawnRadius;
+
     void Update()
     {
         if(timer < timerThreshold)
@@ -19,7 +21,7 @@ public class AppleSpawner : MonoBehaviour
         {
             timer = 0;
             GameObject newlySpawnedApple = Instantiate(applePrefab);
-            newlySpawnedApple.transform.position = new Vector3(Random.Range(-5, 5), 10, Random.Range(-5, 5));
+            newlySpawnedApple.transform.position = new Vector3(Random.Range(spawnRadius.x, spawnRadius.y), transform.position.y, Random.Range(spawnRadius.x, spawnRadius.y));
         }
     }
 }
