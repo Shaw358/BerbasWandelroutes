9404761348699dbe8d04a1e2cd8676546c5d2de6
Robin van den Bor <firelonze2002@outlook.com>
2022-09-01 11:26:30 +0200

    Billboard script + Getallen Sprites

--- Wander route app/Assets/Robin/Script/SomMinigame.cs
+++ Wander route app/Assets/Robin/Script/SomMinigame.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class SomMinigame : MonoBehaviour
 {
     [SerializeField] private GameObject[] numbers;
-    private bool multiplied;
+    [SerializeField] private GameObject multiply;
+    [SerializeField] private GameObject add;
+    [SerializeField] private GameObject wrong;
+    [SerializeField] private TextMeshProUGUI answerfield;
+
+    private bool multiplied = false;
     
     private int numb1;
     private int numb2;
@@ -38,10 +43,28 @@ public class SomMinigame : MonoBehaviour
         if (!multiplied)
         {
             solution = numb1 + numb2;
+            Instantiate(add);
         }
         else
         {
             solution = numb1 * numb2;
+            Instantiate(multiply);
+        }
+    }
+
+    void CheckSolution()
+    {
+        print(int.Parse(answerfield.text));
+        if (int.Parse(answerfield.text) == solution)
+        {
+            //close minigame and give letter
+        }
+        else
+        {
+            //red x over question
+            Instantiate(wrong);
+            answerfield.text = "";
+
         }
     }
 
