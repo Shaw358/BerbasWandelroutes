1dbaa8e2f6a278d01b4b58f01ae285434ab6c67b
CMDR <26555@ma-web.nl>
2022-09-21 01:00:02 +0200

    Last Update

---
+++ Wander route app/Assets/Answer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+    public TextMeshProUGUI mesh;
+    public int answer;
+}
--- Wander route app/Assets/Robin/Script/SomMinigame.cs
+++ Wander route app/Assets/Robin/Script/SomMinigame.cs
@@ -1,73 +1,187 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SomMinigame : MonoBehaviour
 {
     [SerializeField] private GameObject[] numbers;
+    [SerializeField] private GameObject[] types;
     [SerializeField] private GameObject add;
     [SerializeField] private GameObject minus;
-    [SerializeField] private GameObject wrong;
-    [SerializeField] private TextMeshProUGUI answerfield;
+    [SerializeField] private AudioSource wrongSoundEffect;
     [SerializeField] private GameObject endScreen;
 
-    [SerializeField] private TextMeshProUGUI[] answerButtons;
-
-    private GameObject instaNumb1;
-    private GameObject instaNumb2;
-    private GameObject calculationclone;
-
-    private bool additive;
-    private int solutionButtonNumb;
-    private int numb1;
-    private int numb2;
-    private int[] fakeAnswerValue;
-    private int solution;
-    private int usedNumb1;
-    private int usedNumb2;
+    [SerializeField] private Answer[] answerButtons;
+    [SerializeField] TextMeshProUGUI answerfield;
 
     // Start is called before the first frame update
     void Start()
     {
-        Randombool();
-        SetNumbers();
+        SpawnInNumbers();
     }
+
+    bool firstNumberHit;
+    bool secondNumberHit;
+    bool typeHit;
 
     private void FixedUpdate()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 8))
-        {
-            //check if it's number 1 or 2 and show it in bottom images if collided.
-            //zet dit script op camera of zet camera.main voor transform.position
-        }
-    }
-    private void Randombool()
-    {
-        if (Random.value >= .5f)
-        {
-            additive = true;
-        }
-        else
-        {
-            additive = false;
-        }
-    }
-    void SetNumbers()
-    {
-        // X -6 tot 6 meter    Y maximaal 4 meter hoog  Z zelfde als X 
-        //Random.Range(-6, 6), Random.Range(0, 4), Random.Range(-6,6)
-        usedNumb1 = Random.Range(0, numbers.Length);
-        usedNumb2 = Random.Range(0, numbers.Length);
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {}
 
-        instaNumb1 = Instantiate(numbers[usedNumb1], SetPosition(), transform.rotation);
-        instaNumb2 = Instantiate(numbers[usedNumb2], SetPosition(), transform.rotation);
-        numb1 = usedNumb1;
-        numb2 = usedNumb2;
-        FindSolution();
-        FillAnswerButtons();
+        if(hit.transform == null)
+        {
+            return;
+        }
+
+        if (hit.transform.tag == "number1")
+        {
+            firstNumberHit = true;
+        }
+        if (hit.transform.tag == "number2")
+        {
+            secondNumberHit = true;
+        }
+        if (hit.transform.tag == "type")
+        {
+            typeHit = true;
+        }
+
+        string toShow1 = "?";
+        string toShow2 = "?";
+        if (firstNumberHit)
+        {
+            toShow1 = firstNum.ToString();
+        }
+        if (secondNumberHit)
+        {
+            toShow2 = secondNum.ToString();
+        }
+
+        string type = "?";
+        if (typeHit)
+        {
+            switch (somType)
+            {
+                case SomType.plus:
+                    type = "+";
+                    break;
+                case SomType.minus:
+                    type = "-";
+                    break;
+            }
+        }
+        answerfield.text = toShow1 + " " + type + " " + toShow2;
     }
+
+    int firstNum;
+    int secondNum;
+    SomType somType;
+
+    enum SomType
+    {
+        plus,
+        minus
+    }
+
+    GameObject number1;
+    GameObject number2;
+    GameObject type;
+
+    public void SpawnInNumbers()
+    {
+        Destroy(number1);
+        Destroy(number2);
+        Destroy(type);
+
+        somType = (SomType)Random.Range(0, 1);
+
+        switch (somType)
+        {
+            case SomType.plus:
+                firstNum = Random.Range(0, 9);
+                secondNum = Random.Range(0, 9);
+                break;
+            case SomType.minus:
+                firstNum = Random.Range(6, 10);
+                secondNum = Random.Range(1, 6);
+                break;
+            default:
+                break;
+        }
+
+        number1 = Instantiate(numbers[firstNum]);
+        number2 = Instantiate(numbers[secondNum]);
+        type = Instantiate(types[(int)somType]);
+
+        number1.transform.position = SetPosition();
+        number2.transform.position = SetPosition();
+        type.transform.position = SetPosition();
+
+        number1.gameObject.tag = "number1";
+        number2.gameObject.tag = "number2";
+        type.gameObject.tag = "type";
+
+        GenerateAnswers();
+    }
+
+    private void GenerateAnswers()
+    {
+        switch (somType)
+        {
+            case SomType.plus:
+
+                int indexOfRightAnswerPlus = Random.Range(0, 3);
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == indexOfRightAnswerPlus)
+                    {
+                        answerButtons[indexOfRightAnswerPlus].mesh.text = (firstNum + secondNum).ToString();
+                        answerButtons[indexOfRightAnswerPlus].answer = (firstNum + secondNum);
+                    }
+                    else
+                    {
+                        int val1 = Random.Range(0, 9);
+                        int val2 = Random.Range(0, 9);
+                        while ((val1 + val2) == (firstNum + secondNum))
+                        {
+                            val2 = Random.Range(0, 9);
+                        }
+                        int newFakeNumber = val1 + val2;
+                        answerButtons[i].mesh.text = newFakeNumber.ToString();
+                        answerButtons[i].answer = newFakeNumber;
+                    }
+                }
+                break;
+            case SomType.minus:
+                int indexOfRightAnswerMinus = Random.Range(0, 3);
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == indexOfRightAnswerMinus)
+                    {
+                        answerButtons[indexOfRightAnswerMinus].mesh.text = (firstNum - secondNum).ToString();
+                        answerButtons[indexOfRightAnswerMinus].answer = (firstNum - secondNum);
+                    }
+                    else
+                    {
+                        int val1 = Random.Range(6, 10);
+                        int val2 = Random.Range(1, 6);
+                        while ((val1 - val2) == (firstNum - secondNum))
+                        {
+                            val2 = Random.Range(0, 9);
+                        }
+                        int newFakeNumber = val1 + val2;
+                        answerButtons[i].mesh.text = newFakeNumber.ToString();
+                        answerButtons[i].answer = newFakeNumber;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     Vector3 SetPosition()
     {
         int coordx = Random.Range(-7, 7);
@@ -83,69 +197,51 @@ public class SomMinigame : MonoBehaviour
         }
         return new Vector3(coordx, coordy, coordz);
     }
-    void FindSolution()
-    {
-        if (!additive)
-        {
-            solution = numb1 - numb2;
-            Debug.Log(solution + " = " + numb1 + " - " + numb2);
-            calculationclone = Instantiate(minus, SetPosition(), transform.rotation);
-        }
-        else
-        {
-            //solution = numb1 + 1 + numb2 + 1;
-            solution = numb1 + numb2;
-            Debug.Log(solution + " = " + numb1 + " + " + numb2);
-            calculationclone = Instantiate(add, SetPosition(), transform.rotation);
-        }
-    }
+
     public void CheckSolution(int button)
     {
-        if (button == solutionButtonNumb)
+        switch (somType)
         {
-
-        }
-        else
-        {
-            Instantiate(wrong);
-            Destroy(instaNumb1);
-            Destroy(instaNumb2);
-            Destroy(wrong);
-            Randombool();
-            SetNumbers();
+            case SomType.plus:
+                Debug.Log(answerButtons[button].answer + " " + (firstNum + secondNum));
+                if (answerButtons[button].answer == (firstNum + secondNum))
+                {
+                    EndgameScreen();
+                }
+                else
+                {
+                    wrongSoundEffect.Play();
+                    StartCoroutine(ResetMinigame());
+                }
+                break;
+            case SomType.minus:
+                if (answerButtons[button].answer == (firstNum - secondNum))
+                {
+                    EndgameScreen();
+                }
+                else
+                {
+                    wrongSoundEffect.Play();
+                    StartCoroutine(ResetMinigame());
+                }
+                break;
         }
     }
 
-    private void FillAnswerButtons()
+    private IEnumerator ResetMinigame()
     {
-        int answerSlot = Random.Range(1, 4);
-        solutionButtonNumb = answerSlot;
-        for (int i = 0; i < 4; i++)
-        {
-            if (i == answerSlot)
-            {
-                answerButtons[i].text = solution.ToString();
-            }
-            else
-            {
-                fakeAnswerValue[i] = Random.Range(0, 18);
-                if (fakeAnswerValue[i] == solution)
-                {
-                    fakeAnswerValue[i] = Random.Range(0, 18);
-                }
-            }
-        }
-        
+        yield return new WaitForSeconds(1);
+        SpawnInNumbers();
     }
 
     public void BacktoMap()
     {
         SceneManager.LoadSceneAsync(0);
     }
+
     private void EndgameScreen()
     {
         Debug.Log("Reached End");
         endScreen.SetActive(true);
-
     }
 }
